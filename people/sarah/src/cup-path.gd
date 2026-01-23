0e03658fdf606e62a17db99da9a1e9bc2f268ad1
Pixelated Ducky <sarahmroper@proton.me>
2026-01-22 22:17:27 -0500

    feat: cleaner code for disappearing cup

--- people/sarah/src/cup-path.gd
+++ people/sarah/src/cup-path.gd
@@ -6,14 +6,9 @@ var has_reached_end = false
 @onready var body = $StaticBody2D
 
 func _process(delta):
-	if has_reached_end:
-		progress_ratio = 1.0
-
-	progress_ratio += speed * delta * direction
-
 	if progress_ratio >= 1.0:
-		progress_ratio = 1.0
 		has_reached_end = true
 		await get_tree().create_timer(1).timeout
 		queue_free()
-		set_process(false)
+
+	progress_ratio += speed * delta * direction
