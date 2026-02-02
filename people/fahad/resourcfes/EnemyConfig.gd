3c822644ce5951b4ae6eae200b9f1b32b18bb294
Pixelated Ducky <sarahmroper@proton.me>
2026-02-02 11:39:01 -0500

    feat: fahad's code 2

--- people/fahad/resourcfes/EnemyConfig.gd
+++ people/fahad/resourcfes/EnemyConfig.gd
@@ -1,7 +1,7 @@
 extends Resource
+class_name EnemyConfig
 
-@export var sprite: Texture2D
-@export var width: int
-@export var animation_name: String
-@export var points: int
-@export var speed: int
+@export var texture: Texture2D
+@export var speed: float = 10
+@export var health: int = 100
+@export var width: int = 24
--- people/fahad/scrips/enemy_spawner.gd
+++ people/fahad/scrips/enemy_spawner.gd
@@ -1,5 +1,4 @@
 extends Node2D
-
 class_name EnemySpawner
 
 const ROWS = 3
@@ -7,37 +6,39 @@ const COLUMNS = 11
 const HORIZONTAL_SPACING = 80
 const VERTICAL_SPACING = 80
 const ENEMY_HEIGHT = 24
-const START_Y_POSITION = -50
-const ENEMY_POSITION_X_INCREMENT = 10
-const INVADERS_POSITION_Y_INCREMENT = 20
+const START_Y_POSITION = -300
 
-var movement_direction = 1
-var enemy_scene = preload("res://exported/scenes/lvl_1_critter_111.tscn")
+var enemy_scene = preload("res://exported/scenes/Enemy.tscn")
 
 func _ready():
 	var enemy_1_res = preload("res://exported/resourcfes/enemy_1.tres")
-	
-	
-	var enemy_config
-	
-	for row in ROWS:
-		if row == 0:
-			enemy_config = enemy_1_res
-		
-		var row_width = (COLUMNS * enemy_config.width * 3) + ((COLUMNS - 1) * HORIZONTAL_SPACING)
-		var start_x = (position.x - row_width) / 2
-		
-		for col in COLUMNS:
-			var x = start_x + (col * enemy_config.width * 3) + (col * HORIZONTAL_SPACING)
+	var enemy_2_res = preload("res://exported/resourcfes/enemy_2.tres")
+	var enemy_3_res = preload("res://exported/resourcfes/enemy_3.tres")
+
+	for row in range(ROWS):
+		var config_to_use
+
+		match row:
+			0:
+				config_to_use = enemy_1_res
+			1:
+				config_to_use = enemy_2_res
+			2:
+				config_to_use = enemy_3_res
+
+		var row_width = (COLUMNS * config_to_use.width * 3) \
+			+ ((COLUMNS - 1) * HORIZONTAL_SPACING)
+
+		var start_x = position.x - row_width / 2
+
+		for col in range(COLUMNS):
+			var x = start_x + (col * config_to_use.width * 3) + (col * HORIZONTAL_SPACING)
 			var y = START_Y_POSITION + (row * ENEMY_HEIGHT) + (row * VERTICAL_SPACING)
-			var spawn_position = Vector2(x, y)
-			
-			spawn_enemy(enemy_config, spawn_position)
 
-func spawn_enemy(enemy_config, spawn_position: Vector2):
-	var enemy = enemy_scene.instantiate() as Enemy
-	enemy_config = enemy.config
+			spawn_enemy(config_to_use, Vector2(x, y))
+
+func spawn_enemy(config_to_use, spawn_position: Vector2):
+	var enemy = enemy_scene.instantiate()
+	add_child(enemy) # 👈 important
 	enemy.global_position = spawn_position
-	add_child(enemy)
-
-	
+	enemy.config = config_to_use
